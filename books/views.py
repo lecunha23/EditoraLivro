4825a1ba4707088faed4d612393ed85c3e485341
lecunha23 <leandrojc23@gmail.com>
2024-07-02 20:57:57 -0300

    feat: criado arquivo views.py para books

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LivroForm
 
-# Create your views here.
+def livro_create(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('livro_list')  # Substitua 'livro_list' pela URL correta
+    else:
+        form = LivroForm()
+    return render(request, 'books/livro_form.html', {'form': form})
