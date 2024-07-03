848b216f679bcd4ea0e03ba2927cd3dc1a95f5d2
lecunha23 <leandrojc23@gmail.com>
2024-07-02 21:29:11 -0300

    feat: finalinado urls e views

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import livro_create
+from .views import livro_create, livro_list, livro_update, livro_delete
 
 urlpatterns = [
     path('livro/new/', livro_create, name='livro_create'),
+    path('livros/', livro_list, name='livro_list'),
+    path('livro/<int:pk>/edit/', livro_update, name='livro_update'),
+    path('livro/<int:pk>/delete/', livro_delete, name='livro_delete'),
+
+
+
+
     # Adicione outras rotas aqui
 ]
--- books/views.py
+++ books/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import LivroForm
 from .models import Livro
+from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
+from django.contrib import messages
 
 def livro_create(request):
     if request.method == 'POST':
         form = LivroForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('livro_list')  # Substitua 'livro_list' pela URL correta
+            messages.success(request, 'Livro criado com sucesso!')
+            return redirect('livro_list')
     else:
         form = LivroForm()
     return render(request, 'books/livro_form.html', {'form': form})
@@ -15,3 +19,23 @@ def livro_create(request):
 def livro_list(request):
     livros = Livro.objects.all()
     return render(request, 'books/livro_list.html', {'livros': livros})
+
+def livro_update(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        form = LivroForm(request.POST, instance=livro)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Livro atualizado com sucesso!')
+            return redirect('livro_list')
+    else:
+        form = LivroForm(instance=livro)
+    return render(request, 'books/livro_form.html', {'form': form})
+
+
+def livro_delete(request, pk):
+    livro = get_object_or_404(Livro, pk=pk)
+    if request.method == 'POST':
+        livro.delete()
+        return redirect('livro_list')
+    return render(request, 'books/livro_confirm_delete.html', {'livro': livro})
