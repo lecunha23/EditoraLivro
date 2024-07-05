96908b2fcef1a9a4f1d29374d6195146408916d0
lecunha23 <leandrojc23@gmail.com>
2024-07-04 22:53:32 -0300

    Feat: mudando os botao editar e deletar

--- books/models.py
+++ books/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from authors.models import Author
+from .validators import validar_isbn
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     autor = models.ForeignKey(Author, on_delete=models.CASCADE)
-    isbn = models.CharField(max_length=13)
+    isbn = models.CharField(max_length=13, validators=[validar_isbn])
     data_publicacao = models.DateField()
 
 
