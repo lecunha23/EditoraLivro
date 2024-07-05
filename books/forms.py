3c2acaef5ee464aef67fe50fc10a183e3bce09aa
lecunha23 <leandrojc23@gmail.com>
2024-07-04 22:17:44 -0300

    fix: corrigido Urls

--- books/forms.py
+++ books/forms.py
@@ -4,4 +4,4 @@ from .models import Livro
 class LivroForm(forms.ModelForm):
     class Meta:
         model = Livro
-        fields = ['titulo', 'autor', 'isbn', 'data_publicacao', 'preco']
+        fields = ['titulo', 'autor', 'isbn', 'data_publicacao',]
--- books/urls.py
+++ books/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('livro/new/', livro_create, name='livro_create'),
     path('livros/', livro_list, name='livro_list'),
+    path('livros/<int:pk>/', views.livro_detail, name='livro_detail'),
     path('livro/<int:pk>/edit/', livro_update, name='livro_update'),
     path('livro/<int:pk>/delete/', livro_delete, name='livro_delete'),
 
