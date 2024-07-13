e75bc873c04c6080467fec391b586b0aaed27b4a
lecunha23 <leandrojc23@gmail.com>
2024-07-13 16:55:49 -0300

    feat: adicionado campo valor das peças

--- pecas/forms.py
+++ pecas/forms.py
@@ -4,4 +4,4 @@ from .models import Peca
 class PecaForm(forms.ModelForm):
     class Meta:
         model = Peca
-        fields = ['nome', 'descricao', 'fornecedor']
+        fields = ['nome', 'descricao', 'fornecedor', 'valor']
--- pecas/models.py
+++ pecas/models.py
@@ -5,6 +5,7 @@ class Peca(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField(null=True, blank=True)
     fornecedor = models.ForeignKey(Fornecedor, on_delete=models.CASCADE)
+    valor = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
     def __str__(self):
         return self.nome
